e9805089ce09cbf933a6f065cf6398cbfda8c654
PivovarovIV <brauer.igor@gmail.com>
2024-01-13 03:30:22 +0300

    Для платформы Android на экранe деталей исправлен баг, где исключено множественное нажатие кнопки бэк

--- androidScramdinger/src/main/java/ru/brauer/scrumdinger/android/DetailsView.kt
+++ androidScramdinger/src/main/java/ru/brauer/scrumdinger/android/DetailsView.kt
@@ -30,6 +30,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -65,6 +69,7 @@ private fun AppBar(scrum: DailyScrum, navController: NavHostController) {
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
+            var isEnabled by remember { mutableStateOf(true) }
             LargeTopAppBar(
                 colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = MaterialTheme.colorScheme.background
@@ -78,7 +83,11 @@ private fun AppBar(scrum: DailyScrum, navController: NavHostController) {
                 },
                 navigationIcon = {
                     IconButton(
-                        onClick = { navController.popBackStack() }
+                        onClick = {
+                            isEnabled = false
+                            navController.popBackStack()
+                        },
+                        enabled = isEnabled
                     ) {
                         Icon(
                             imageVector = Icons.Filled.ArrowBackIos,
